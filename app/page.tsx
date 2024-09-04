6372076242f5855f0cc23933cb3ec342dbbb4c0a
mohamed <mohamedabdelwahidi@gmail.com>
2024-09-04 12:20:29 +0100

    new edite from simo

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <h1>Ok fine</h1>
+          <h1>Ok fine form simo visual studio</h1>
         </div>
       </div>
 
